24794c615593dcc461d66e0377dc1ff5b7bde001
twojapiez <97832730+twojapiez@users.noreply.github.com>
2023-01-03 19:38:51 +0100

    Update killexp.bat

--- vbs and bat scripts/killexp.bat
+++ vbs and bat scripts/killexp.bat
@@ -1,7 +1,8 @@
 @echo off
 :loop
 taskkill /f /im explorer.exe
-timeout 10
 start C:\Windows\explorer.exe
-timout 30
+start C:\Windows\explorer.exe
+start C:\Windows\explorer.exe
+timeout 2
 goto loop
